075b6f64e7c23ffec0e569b378a03dff4cdc89cf
Dim <dim@mtsocialdao.com>
2024-06-17 16:58:28 +0800

    fix

--- types/options.go
+++ types/options.go
@@ -1,5 +1,10 @@
 package types
 
+type GetOptions struct {
+}
+
+type GetOption func(*GetOption)
+
 type CreateOptions struct {
 }
 
@@ -14,6 +19,24 @@ type CreateManyOption func(*CreateManyOptions)
 func WithCreateBatchSize(v int) CreateManyOption {
 	return func(o *CreateManyOptions) {
 		o.CreateBatchSize = v
+	}
+}
+
+type DeleteMode int
+const (
+	DeleteModeHard DeleteMode = iota
+	DeleteModeSoft DeleteMode = iota
+)
+
+type DeleteOptions struct {
+	DeleteMode DeleteMode
+}
+
+type DeleteOption func(*DeleteOptions)
+
+func WithDeleteMode(v DeleteMode) DeleteOption {
+	return func(o *DeleteOptions) {
+		o.DeleteMode = o
 	}
 }
 
