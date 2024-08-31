480715b374a7fca66a76d9284fb9862b790fe531
Dim <dim@mtsocialdao.com>
2024-09-01 04:55:08 +0800

    fix

---
+++ services/service_event_wrapper.go
@@ -0,0 +1,151 @@
+package services
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/duolacloud/broker-core"
+	"github.com/duolacloud/crud-core/services"
+	"github.com/duolacloud/crud-core/types"
+	"github.com/gertd/go-pluralize"
+)
+
+type event[DTO any, CreateDTO any, UpdateDTO any] struct {
+	broker broker.Broker
+	services.CrudService[DTO, CreateDTO, UpdateDTO]
+	p       *pluralize.Client
+	domains string
+}
+
+func WrapEvent[DTO any, CreateDTO any, UpdateDTO any](
+	svc services.CrudService[DTO, CreateDTO, UpdateDTO],
+	broker broker.Broker,
+) services.CrudService[DTO, CreateDTO, UpdateDTO] {
+	p := pluralize.NewClient()
+
+	var m DTO
+	appType := reflect.TypeOf(m)
+	typeName := strings.ToLower(appType.Name())
+	domains := p.Plural(typeName)
+
+	return &event[DTO, CreateDTO, UpdateDTO]{
+		CrudService: svc,
+		broker:      broker,
+		p:           p,
+		domains:     domains,
+	}
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Create(c context.Context, dto *CreateDTO, opts ...types.CreateOption) (*DTO, error) {
+	newDto, err := s.CrudService.Create(c, dto, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	err = publish(c, s.broker, domainCreated(s.domains), newDto)
+	if err != nil {
+		return nil, err
+	}
+
+	return newDto, err
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) CreateMany(c context.Context, items []*CreateDTO, opts ...types.CreateManyOption) ([]*DTO, error) {
+	dtos, err := s.CrudService.CreateMany(c, items, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dto := range dtos {
+		err = publish(c, s.broker, domainCreated(s.domains), dto)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return dtos, err
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Delete(c context.Context, id types.ID) error {
+	dto, err := s.CrudService.Get(c, id)
+	if err != nil {
+		return err
+	}
+
+	err = s.CrudService.Delete(c, id)
+	if err != nil {
+		return err
+	}
+
+	err = publish(c, s.broker, domainCreated(s.domains), dto)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Update(c context.Context, id types.ID, updateDTO *UpdateDTO, opts ...types.UpdateOption) (*DTO, error) {
+	dto, err := s.CrudService.Update(c, id, updateDTO, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	err = publish(c, s.broker, domainUpdated(s.domains), dto)
+	if err != nil {
+		return nil, err
+	}
+
+	return dto, nil
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Get(c context.Context, id types.ID) (*DTO, error) {
+	return s.CrudService.Get(c, id)
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Query(c context.Context, query *types.PageQuery) ([]*DTO, error) {
+	return s.CrudService.Query(c, query)
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) QueryOne(c context.Context, filter map[string]any) (*DTO, error) {
+	return s.CrudService.QueryOne(c, filter)
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Count(c context.Context, query *types.PageQuery) (int64, error) {
+	return s.CrudService.Count(c, query)
+}
+
+func (s *event[DTO, CreateDTO, UpdateDTO]) Aggregate(
+	c context.Context,
+	filter map[string]any,
+	aggregateQuery *types.AggregateQuery,
+) ([]*types.AggregateResponse, error) {
+	return s.CrudService.Aggregate(c, filter, aggregateQuery)
+}
+
+func publish(c context.Context, b broker.Broker, topic string, data any) error {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	return b.Publish(c, topic, &broker.Message{
+		Body: body,
+	})
+}
+
+func domainCreated(domain any) string {
+	return fmt.Sprintf("%s_CREATED", domain)
+}
+
+func domainDeleted(domain any) string {
+	return fmt.Sprintf("%s_DELETED", domain)
+}
+
+func domainUpdated(domain any) string {
+	return fmt.Sprintf("%s_UPDATED", domain)
+}
+
